3764ad72e497a11dd9d9e93c233463eb8339308a
Rizchel Wood <rizchelwood@gmail.com>
2019-06-20 11:11:52 -0600

    Upgrade to gatsby v2

--- src/components/blog.js
+++ src/components/blog.js
@@ -1,52 +1,48 @@
 import React from 'react'
-import Link from 'gatsby-link'
+import { graphql } from 'gatsby'
 import renderHTML from 'react-render-html'
 
+import Layout from './layout'
 
-export default class BlogPage extends React.Component {
-    render() {
-        const { data } = this.props;
-        const blog = data.contentstackBlogs;
-        return (
-            <div className="container">
-                <div>
-                    <section>
-                        <div className="blog-title">
-                            <h2>{blog.title}</h2>
-                        </div>
-                        <div className="blog-description">
-                            {renderHTML(blog.description)}
-                        </div>
-                        <div className="author">
-                            By: {blog.authors.map(author => {
-                                return(
-                                    <span key={author.id}>
-                                        {author.name}
-                                    </span>
-                                )
-                            })}
-                        </div>
-                    </section>
-                </div>
+const BlogPage = ({ data }) => {
+  const blog = data.contentstackBlogs
+  return (
+    <Layout>
+      <div className="container">
+        <div>
+          <section>
+            <div className="blog-title">
+              <h2>{blog.title}</h2>
             </div>
-        )
-    }
+            <div className="blog-description">
+              {renderHTML(blog.description)}
+            </div>
+            <div className="author">
+              By:{' '}
+              {blog.authors.map(author => {
+                return <span key={author.id}>{author.name}</span>
+              })}
+            </div>
+          </section>
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
+export default BlogPage
 
 export const pageQuery = graphql`
-    query BlogPostByID($id: String!) {
-      contentstackBlogs(id: {eq: $id}) {
-	    title
-	    id
-	    url
-	    description
-	    authors {
-	   	  id 	
-	      name
-	    }
-	  }
+  query BlogPostByID($id: String!) {
+    contentstackBlogs(id: { eq: $id }) {
+      title
+      id
+      url
+      description
+      authors {
+        id
+        name
+      }
     }
-
+  }
 `
-
