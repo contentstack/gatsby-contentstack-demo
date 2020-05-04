351b9f2d1b3388b0f775a86b8e1d56b1982b7783
asmit-patil <patilasmit0@gmail.com>
2020-05-04 11:09:09 +0530

    updated module version and config options

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,13 @@ module.exports = {
       options:{
         'api_key':'your_api_key',
         'delivery_token':'your_delivery_token',
-        'environment':'your_development'
+        'environment':'your_development',
+        // Optional: expediteBuild set this to either true or false
+        'expediteBuild': `true`,
+        // Optional: Specify true if you want to generate custom schema
+        'enableSchemaGeneration' : `true`,
+        // Optional: Specify a different prefix for types. This is useful in cases where you have multiple instances of the plugin to be connected to different stacks.
+        'type_prefix': `Contentstack`, // (default)
       },
     },
     'gatsby-plugin-react-helmet'
